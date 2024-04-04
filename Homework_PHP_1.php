fb374acca3b93ac94d487f8fd4f71a0d371a4fc9
RenK_RAP <131636662+RenKRAP@users.noreply.github.com>
2024-04-04 15:41:14 +0900

    updating HW1

--- Homework_PHP_1.php
+++ Homework_PHP_1.php
@@ -7,19 +7,26 @@
     <body>
         <h1>This is HW1</h1>
         <h2>합, 곱 구하기</h2>
-        <?php
-        echo "My first PHP echo!<br>";
-        $n = 10;
-        $total_plus = 0;
-        $total_times = 1;
-        for ($i = 1; $i<$n+1; $i++) {
-            echo "$i ";
-            $total_plus = $total_plus + $i;
-            $total_times = $total_times * $i;
+        <form method="post" action="">
+            <label for="number1">n : </label>
+            <input type="number" name="number1" id="number1">
+            <br><br>
+            <input type="submit" value="출력">
+        </form>
+       <?php
++       $n = $_POST["number1"];
+        if(isset($_POST["number1"])){
+-           $n = $_POST["number1"];
+            $total_plus = 0;
+            $total_times = 1;
+            for ($i = 1; $i<$n+1; $i++) {
+                echo "$i ";
+                $total_plus = $total_plus + $i;
+                $total_times = $total_times * $i;
+            } 
+            echo "<br>$n 까지의 합은 : $total_plus";
+            echo "<br>$n 까지의 곱은 : $total_times";
         }
-        echo "<br>$n 까지의 합은 : $total_plus";
-        echo "<br>$n 까지의 곱은 : $total_times";
-
         ?>
     </body>
 </html>
