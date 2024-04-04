d064cebad633deb398c9336823ed17e4bbf1e5f1
RenK_RAP <131636662+RenKRAP@users.noreply.github.com>
2024-04-04 11:06:20 +0900

    Homework first

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE>
+<html>
+    <head>
+
+    </head>
+    
+    <body>
+        <?php
+        echo "My first PHP echo!<br>";
+        $n = 30;
+        $total_plus = 0;
+        for ($i = 1; $i<$n+1; $i++) {
+            echo "$i ";
+            $total_plus = 
+        }
+
+        ?>
+    </body>
+</html>
